226fff6af97a47e502a54677c32ba02611bec55b
suehtamCruz <matheuscz3110@gmail.com>
2020-12-17 15:28:48 -0300

    UPDATE : Home e ação finalizados e responsivos

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const $btnMenuOpen = document.querySelector('.btnMenu_open')
 const $menu = document.querySelector('.menu-mob')
 const $btnMenuClose = document.querySelector('.btnMenu_close')
+const $container = document.querySelector('.container')
 
 $btnMenuOpen.addEventListener('click',function(){
     $menu.classList.add('menu-mob_open')    
+    $container.classList.add('container_down');
 })
 
 
 $btnMenuClose.addEventListener('click',function(){
     $menu.classList.remove('menu-mob_open')    
+    $container.classList.remove('container_down');
 })
